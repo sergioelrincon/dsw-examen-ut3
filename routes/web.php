d70f1b93eb0b82aac8c11edffb802a97c2620789
Sergio Ramos <sergioramos@ieselrincon.es>
2025-11-20 13:49:03 +0000

    Añadimos la ruta /

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::post('/product', [ProductController::class, 'store'])->name('product.store');
